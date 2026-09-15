c12f36deae861875bb952b654aa5f21c3532934c
nicolasgalvis1601-maker <nicolasgalvis1601@gmail.com>
2026-09-15 10:56:57 -0500

    Add files via upload

---
+++ src/components/TeamMemberCard.vue
@@ -0,0 +1,13 @@
+<template>
+  <q-card flat class="team-card">
+    <q-img :src="member.image" ratio="1" />
+    <q-card-section>
+      <div class="team-role">{{ member.role }}</div>
+      <div class="team-name">{{ member.name }}</div>
+    </q-card-section>
+  </q-card>
+</template>
+
+<script setup>
+defineProps(['member'])
+</script>
